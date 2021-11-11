65b2282bb7c773cc3462a55f855df92779c1ca25
kmiu2 <kennymiu@rogers.com>
2021-11-11 15:53:05 -0500

    preloading and font optimizations

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
