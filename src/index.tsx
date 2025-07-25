65c879f186615c6600454181544066958c1f73fd
kmiu2 <kennymiu@rogers.com>
2025-07-24 21:40:43 -0400

    feat: upgrade to react 18 and improve build scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-ReactDOM.render(
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
 
 serviceWorkerRegistration.register()
