48529693a0b405d888e06cbd86ea6867b26580f4
kmiu2 <kennymiu@rogers.com>
2025-07-24 20:54:50 -0400

    fix: add static HTML form for Netlify form detection

--- src/components/main/ContactForm.tsx
+++ src/components/main/ContactForm.tsx
@@ -3,6 +3,7 @@ import { Button, Form } from 'react-bootstrap'
 export function ContactForm() {
   return (
     <form name="contact" method="POST" data-netlify="true" className="contactForm">
+      <input type="hidden" name="form-name" value="contact" />
       <Form.Group className="mb-3">
         <Form.Label>Name</Form.Label>
         <Form.Control type="text" name="name" required />
