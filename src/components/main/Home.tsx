c2789a390b5d8f2c95cd2ff84742676be473137e
kmiu2 <kennymiu@rogers.com>
2025-08-05 23:09:48 -0400

    fix: small profile picture loading

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -9,14 +9,14 @@ export function Home() {
   const profilePicture = () => (
     <picture>
       <source
-        srcSet="/images/me-small.webp 500w, /images/me.webp 1000w"
+        srcSet="/images/me-small.webp 600w, /images/me.webp 1000w"
         sizes="(max-width: 768px) 80vw, 500px"
         type="image/webp"
       />
       <img
         className="imgWrapper"
         src="/images/me.jpg"
-        srcSet="/images/me-small.jpg 500w, /images/me.jpg 1000w"
+        srcSet="/images/me-small.jpg 600w, /images/me.jpg 1000w"
         sizes="(max-width: 768px) 80vw, 500px"
         alt="me"
         width="1000"
